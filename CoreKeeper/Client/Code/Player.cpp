ba2c9ea53eb1f3ffaa22ee9e887140ae85b70af8
danggunee <tkddms6161@gmail.com>
2024-09-08 22:58:14 +0900

    Player KeyInput Right 주석 해제, Player 방향 전환 코드 추가

--- CoreKeeper/Client/Code/Player.cpp
+++ CoreKeeper/Client/Code/Player.cpp
@@ -94,14 +94,14 @@ void CPlayer::Key_Input(const _float& fTimeDelta)
 	}
 	else if (GetAsyncKeyState(VK_LEFT))
 	{
-		//m_pTransformCom->Rotation(ROT_Y, D3DXToRadian(-180.f * fTimeDelta));
+		m_pTransformCom->Rotation(ROT_Y, D3DXToRadian(-180.f * fTimeDelta));
 		m_pAnimatorCom->Set_CurState(WALK, 7, 10, 15);
 	}
-	//else if (GetAsyncKeyState(VK_RIGHT))
-	//{
-	//	//m_pTransformCom->Rotation(ROT_Y, D3DXToRadian(180.f * fTimeDelta));
-	//	m_pAnimatorCom->Set_CurState(WALK, 7, 10, 15);
-	//}
+	else if (GetAsyncKeyState(VK_RIGHT))
+	{
+		m_pTransformCom->Rotation(ROT_Y, D3DXToRadian(180.f * fTimeDelta));
+		m_pAnimatorCom->Set_CurState(WALK, 7, 10, 15);
+	}
 	else
 	{
 		m_pAnimatorCom->Set_CurState(IDLE, 0, 0, 15);
