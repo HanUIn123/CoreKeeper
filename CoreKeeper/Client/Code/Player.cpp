056add258fc943c7b3fe333f8308d98030ec66bf
DESKTOP-MH8CQTR\sangh <sanghyeogim323@gmail.com>
2024-09-08 23:29:30 +0900

    5 -> 20

--- CoreKeeper/Client/Code/Player.cpp
+++ CoreKeeper/Client/Code/Player.cpp
@@ -84,12 +84,12 @@ void CPlayer::Key_Input(const _float& fTimeDelta)
 
 	if (GetAsyncKeyState(VK_UP))
 	{
-		m_pTransformCom->Move_Pos(D3DXVec3Normalize(&vLook, &vLook), fTimeDelta, 5.f);
+		m_pTransformCom->Move_Pos(D3DXVec3Normalize(&vLook, &vLook), fTimeDelta, 20.f);
 		m_pAnimatorCom->Set_CurState(WALK, 11, 14, 15);
 	}
 	else if (GetAsyncKeyState(VK_DOWN))
 	{
-		m_pTransformCom->Move_Pos(D3DXVec3Normalize(&vLook, &vLook), fTimeDelta, -5.f);
+		m_pTransformCom->Move_Pos(D3DXVec3Normalize(&vLook, &vLook), fTimeDelta, -20.f);
 		m_pAnimatorCom->Set_CurState(WALK, 3, 6, 15);
 	}
 	else if (GetAsyncKeyState(VK_LEFT))
