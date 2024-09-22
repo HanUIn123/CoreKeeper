94b722f82a3c71c7e98c17c0a0b401a3f20ec7b7
danggunee <tkddms6161@gmail.com>
2024-09-22 20:48:36 +0900

    트랜스폼 수정

--- CoreKeeper/Reference/Header/Transform.h
+++ CoreKeeper/Reference/Header/Transform.h
@@ -60,8 +60,6 @@ public:
 	void				Chase_Target(const _vec3* pTargetPos, const _float& fSpeed);
 	const _matrix*		Compute_LookAtTarget(const _vec3* pTargetPos);
 
-	void				Update_Matrix();
-
 public:
 	_vec3		m_vInfo[INFO_END];
 	_vec3		m_vScale;
