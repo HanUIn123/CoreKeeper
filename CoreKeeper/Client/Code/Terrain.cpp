a6d37975a6a8bd8ce7f6bcaaa8b416ea086e90c2
danggunee <tkddms6161@gmail.com>
2024-09-25 16:10:31 +0900

    터레인 수정

--- CoreKeeper/Client/Code/Terrain.cpp
+++ CoreKeeper/Client/Code/Terrain.cpp
@@ -5,6 +5,7 @@
 CTerrain::CTerrain(LPDIRECT3DDEVICE9 pGraphicDev)
 	: Engine::CGameObject(pGraphicDev)
 {
+	m_vecTextureNumber.resize((VTXCNTX - 1) * (VTXCNTZ - 1));
 }
 
 CTerrain::~CTerrain()
@@ -38,9 +39,17 @@ void CTerrain::Render_GameObject()
 
 	FAILED_CHECK_RETURN(Setup_Material(), );
 
-	m_pTextureCom->Set_Texture(0);
+	for (int i = 0; i < VTXCNTZ - 1; ++i)
+	{
+		for (int j = 0; j < VTXCNTX - 1; ++j)
+		{
+			int idx = i * (VTXCNTX - 1) + j;
 
-	m_pBufferCom->Render_Buffer();
+			auto texture = m_pTextureCom->Get_Texture(m_vecTextureNumber[idx]);
+			m_pGraphicDev->SetTexture(0, texture);
+			m_pBufferCom->Render_Texture(idx * 6);
+		}
+	}
 
 	m_pGraphicDev->SetRenderState(D3DRS_LIGHTING, FALSE);
 	m_pGraphicDev->SetRenderState(D3DRS_CULLMODE, D3DCULL_CCW);
