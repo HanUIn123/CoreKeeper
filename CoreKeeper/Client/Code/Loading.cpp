4a1ff5103038ea92624c26fd9bc83c8dca73eae7
danggunee <tkddms6161@gmail.com>
2024-10-06 14:20:11 +0900

    도시락, 초코바 음식 추가

--- CoreKeeper/Client/Code/Loading.cpp
+++ CoreKeeper/Client/Code/Loading.cpp
@@ -203,6 +203,8 @@ _uint CLoading::Loading_Stage()
     FAILED_CHECK_RETURN(Engine::Ready_Proto(L"Proto_PotionTexture", Engine::CTexture::Create(m_pGraphicDev, L"../Bin/Resource/Texture/Item/Potion/Potion_%d.png", TEX_NORMAL, 3)), E_FAIL);
     FAILED_CHECK_RETURN(Engine::Ready_Proto(L"Proto_IngredientTexture", Engine::CTexture::Create(m_pGraphicDev, L"../Bin/Resource/Texture/Item/Ingredient/Ingredient_%d.png", TEX_NORMAL, 5)), E_FAIL);
     FAILED_CHECK_RETURN(Engine::Ready_Proto(L"Proto_FoodTexture", Engine::CTexture::Create(m_pGraphicDev, L"../Bin/Resource/Texture/Item/Food/Food_%d.png", TEX_NORMAL, 10)), E_FAIL);
+    FAILED_CHECK_RETURN(Engine::Ready_Proto(L"Proto_LunchTexture", Engine::CTexture::Create(m_pGraphicDev, L"../Bin/Resource/Texture/Item/Food/Lunch.png", TEX_NORMAL)), E_FAIL);
+    FAILED_CHECK_RETURN(Engine::Ready_Proto(L"Proto_ChocoBarTexture", Engine::CTexture::Create(m_pGraphicDev, L"../Bin/Resource/Texture/Item/Food/ChocoBar.png", TEX_NORMAL)), E_FAIL);
 
     // 기타 아이템
     FAILED_CHECK_RETURN(Engine::Ready_Proto(L"Proto_OreTexture", Engine::CTexture::Create(m_pGraphicDev, L"../Bin/Resource/Texture/Item/Ore/Ore_%d.png", TEX_NORMAL, 3)), E_FAIL);
---
+++ CoreKeeper/Client/Code/Lunch.cpp
@@ -0,0 +1,176 @@
+#include "pch.h"
+#include "..\Header\Lunch.h"
+#include "Export_System.h"
+#include "Export_Utility.h"
+
+CLunch::CLunch(LPDIRECT3DDEVICE9 pGraphicDev)
+	: CItem(pGraphicDev)
+{
+	m_eItemNum = ITEM_LUNCH;
+}
+
+CLunch::~CLunch()
+{
+}
+
+HRESULT CLunch::Ready_GameObject(_vec3 vPos)
+{
+	FAILED_CHECK_RETURN(Add_Component(), E_FAIL);
+
+	m_pTransformCom->Set_Scale(0.2f, 0.2f, 0.2f);
+	m_pShadowTransformCom->Set_Scale(0.2f, 0.2f, 0.2f);
+
+	m_pTransformCom->Set_Pos(vPos.x, vPos.y, vPos.z);
+	m_pShadowTransformCom->Set_Pos(vPos.x, 0.1f, vPos.z);
+
+	m_wItemName = L"도시락";
+	m_wItemExplain[0] = L"어머니가 싸준 도시락입니다.";
+	m_wItemExplain[1] = L"한입 먹을 때마다 어머니의 사랑이 느껴집니다.";
+
+	// 원래의 Y 위치 저장
+	m_fFirstY = vPos.y;
+
+	return S_OK;
+}
+
+_int CLunch::Update_GameObject(const _float& fTimeDelta)
+{
+	_vec3 vPos;
+	m_pTransformCom->Get_Info(INFO_POS, &vPos);
+	m_pShadowTransformCom->Set_Pos(vPos.x, 0.1f, vPos.z);
+
+	if (m_bUse)
+	{
+		m_bActive = true;
+		m_bDrop = false;
+		m_pTransformCom->Set_Scale(0.2f, 0.2f, 0.2f);
+	}
+
+	if (m_bDrop)
+	{
+		// 아이템 움직임
+		CItem::Wave(fTimeDelta);
+
+		Engine::CCollider* pPlayerCollider = dynamic_cast<Engine::CCollider*>
+			(Engine::Get_Component(ID_DYNAMIC, L"Layer_GameLogic", L"Player", L"Com_Collider"));
+
+		// 플레이어와 충돌
+		if (m_pColliderCom->Check_Collision(pPlayerCollider))
+		{
+			Engine::CInventory* pPlayerInventory = dynamic_cast<Engine::CInventory*>
+				(Engine::Get_Component(ID_STATIC, L"Layer_GameLogic", L"Player", L"Com_Inventory"));
+		
+			// 인벤토리에 들어갔다
+			if (pPlayerInventory->Add_Item(this))
+			{
+				m_bActive = false;
+				m_bDrop = false;
+			}
+		}
+	}
+	
+	Add_RenderGroup(RENDER_ALPHA, this);
+
+	return Engine::CGameObject::Update_GameObject(fTimeDelta);
+}
+
+void CLunch::LateUpdate_GameObject()
+{
+	Engine::CGameObject::LateUpdate_GameObject();
+}
+
+void CLunch::Render_GameObject()
+{
+	m_pGraphicDev->SetRenderState(D3DRS_LIGHTING, TRUE);
+	m_pGraphicDev->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
+
+	FAILED_CHECK_RETURN(Setup_Material(), );
+
+	m_pGraphicDev->SetTransform(D3DTS_WORLD, m_pTransformCom->Get_WorldMatrix());
+	m_pColliderCom->Update_Collider(m_pTransformCom->Get_WorldMatrix());
+
+	m_pTextureCom->Set_Texture();
+
+	m_pBufferCom->Set_Index(0);
+
+	if (m_bActive)
+	{
+		m_pBufferCom->Render_Buffer();
+	}
+
+	// 콜라이더 렌더링 추가
+	if (m_bActive)
+	{
+		m_pColliderCom->Render_Collider();
+	}
+
+	m_pGraphicDev->SetTransform(D3DTS_WORLD, m_pShadowTransformCom->Get_WorldMatrix());
+
+	m_pShadowTextureCom->Set_Texture(1);
+
+	if (m_bDrop)
+	{
+		m_pShadowBufferCom->Render_Buffer();
+	}
+
+	m_pGraphicDev->SetRenderState(D3DRS_LIGHTING, FALSE);
+	m_pGraphicDev->SetRenderState(D3DRS_CULLMODE, D3DCULL_CCW);
+}
+
+HRESULT CLunch::Add_Component()
+{
+	CComponent* pComponent = NULL;
+
+	pComponent = m_pBufferCom = dynamic_cast<CAnimTex*>(Engine::Clone_Proto(L"Proto_NormalAnimTex"));
+	NULL_CHECK_RETURN(pComponent, E_FAIL);
+	m_mapComponent[ID_STATIC].insert({ L"Com_Buffer", pComponent });
+
+	pComponent = m_pTextureCom = dynamic_cast<CTexture*>(Engine::Clone_Proto(L"Proto_LunchTexture"));
+	NULL_CHECK_RETURN(pComponent, E_FAIL);
+	m_mapComponent[ID_STATIC].insert({ L"Com_Texture", pComponent });
+
+	pComponent = m_pTransformCom = dynamic_cast<CTransform*>(Engine::Clone_Proto(L"Proto_Transform"));
+	NULL_CHECK_RETURN(pComponent, E_FAIL);
+	m_mapComponent[ID_DYNAMIC].insert({ L"Com_Transform", pComponent });
+
+	pComponent = m_pColliderCom = dynamic_cast<CCollider*>(Engine::Clone_Proto(L"Proto_ItemCollider"));
+	NULL_CHECK_RETURN(pComponent, E_FAIL);
+	m_mapComponent[ID_DYNAMIC].insert({ L"Com_Collider", pComponent });
+
+	pComponent = m_pAnimatorCom = dynamic_cast<CAnimator*>(Engine::Clone_Proto(L"Proto_Animator"));
+	NULL_CHECK_RETURN(pComponent, E_FAIL);
+	m_mapComponent[ID_DYNAMIC].insert({ L"Com_Animator", pComponent });
+
+	pComponent = m_pShadowBufferCom = dynamic_cast<CShadowTex*>(Engine::Clone_Proto(L"Proto_ShadowTex"));
+	NULL_CHECK_RETURN(pComponent, E_FAIL);
+	m_mapComponent[ID_DYNAMIC].insert({ L"Com_ShadowCom", pComponent });
+
+	pComponent = m_pShadowTextureCom = dynamic_cast<CTexture*>(Engine::Clone_Proto(L"Proto_ShadowTexture"));
+	NULL_CHECK_RETURN(pComponent, E_FAIL);
+	m_mapComponent[ID_DYNAMIC].insert({ L"Com_ShadowTexture", pComponent });
+
+	pComponent = m_pShadowTransformCom = dynamic_cast<CTransform*>(Engine::Clone_Proto(L"Proto_Transform"));
+	NULL_CHECK_RETURN(pComponent, E_FAIL);
+	m_mapComponent[ID_DYNAMIC].insert({ L"Com_ShadowTransform", pComponent });
+
+	return S_OK;
+}
+
+CLunch* CLunch::Create(LPDIRECT3DDEVICE9 pGraphicDev, _vec3 vPos)
+{
+	CLunch* pLunch = new CLunch(pGraphicDev);
+
+	if (FAILED(pLunch->Ready_GameObject(vPos)))
+	{
+		Safe_Release(pLunch);
+		MSG_BOX("pLunch Create Failed");
+		return nullptr;
+	}
+
+	return pLunch;
+}
+
+void CLunch::Free()
+{
+	Engine::CGameObject::Free();
+}
