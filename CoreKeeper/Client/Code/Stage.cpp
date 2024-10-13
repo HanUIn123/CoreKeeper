21ba136d08901c466faa5ba02db695d976b467f9
HanUIn123 <gksyy12345@gmail.com>
2024-10-13 16:06:06 +0900

    아제오스 그림자 추가

--- CoreKeeper/Client/Code/Stage.cpp
+++ CoreKeeper/Client/Code/Stage.cpp
@@ -278,6 +278,10 @@ HRESULT CStage::Ready_Layer_Environment(const _tchar* pLayerTag)
     FAILED_CHECK_RETURN(pLayer->Add_GameObject(L"AzeosSummonPoint", pGameObject), E_FAIL);
 
 
+
+
+
+
 #pragma endregion
 
     m_mapLayer.insert({ pLayerTag , pLayer });
@@ -347,6 +351,13 @@ HRESULT CStage::Ready_Layer_GameLogic(const _tchar* pLayerTag)
 
 #pragma endregion
 
+
+    // 아제오스 그릠자 
+    // 45.0f, -0.2f, 106.0
+    _vec3   AzeosShadowPos = { 45.0f, 0.2f, 106.0f };
+    pGameObject = CAzeosShadow::Create(m_pGraphicDev, AzeosShadowPos);
+    NULL_CHECK_RETURN(pGameObject, E_FAIL);
+    FAILED_CHECK_RETURN(pLayer->Add_GameObject(L"AzeosShadow", pGameObject), E_FAIL);
 
     m_mapLayer.insert({ pLayerTag , pLayer });
 
